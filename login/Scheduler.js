afa53485e8d75eddc86870ab1990dd77339585e7
Ali zoghi <azouqi@gmail.com>
2016-10-03 00:24:04 -0400

    drag is done. confirmation pop up dialogue is done.

---
+++ login/Scheduler.js
@@ -0,0 +1,56 @@
+$(function () {
+      var isMouseDown = false;
+	  var $startDate;
+	  var $endDate;
+      $("#scheduler td")
+        .mousedown(function () {
+          isMouseDown = true;
+		  if($(this).attr("select")=="T"){
+			  $(this).attr("select", "F");
+			  $startDate=null;
+		  }else{
+			  $(this).attr("select", "T");
+			  $startDate=($(this).html());
+		  }
+		  console.log($(this).attr("select"));
+          $(this).toggleClass("highlighted");
+          return false; // prevent text selection
+        })
+        .mouseover(function () {
+          if (isMouseDown) {
+			if($(this).attr("select")=="T"){
+			  $(this).attr("select", "F");
+			}else{
+			  $(this).attr("select", "T");
+			  if($startDate==null){
+				  $startDate=($(this).html());
+			  }
+			}
+            $(this).toggleClass("highlighted");
+          }
+        })
+		.mouseup(function(){
+			if($(this).attr("select")=="T"){
+				$endDate=($(this).html());
+				console.log($startDate);
+				console.log($endDate);
+				if((parseInt($startDate))>(parseInt($endDate))){
+					var temp=$startDate;
+					$startDate=$endDate;
+					$endDate=temp;
+				}else if((parseInt($startDate))==(parseInt($endDate))){
+					bootbox.confirm(("You want to select "+$startDate+" o'clock?"), function(result){ console.log('This was logged in the callback: ' + result); });
+				}else{
+					bootbox.confirm(("You want to select from "+$startDate+ " o'clock to "+ $endDate+" o'clock?"), function(result){ console.log('You should write the save to DB stuff ' + result); });
+				}
+			}
+		})
+        .bind("selectstart", function () {
+          return false; // prevent text selection in IE
+        });
+
+      $(document)
+        .mouseup(function () {
+          isMouseDown = false;
+        });
+    });
